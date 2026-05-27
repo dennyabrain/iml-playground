290a5e5cd93ec4c5e058b5d1496c7ce2130e6b14
dennyabrain <denny.george90@gmail.com>
2026-05-27 16:43:50 +0530

    feat: add github action for pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_URL ?? '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
