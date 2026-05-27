66d9402b4b603ca022a39f39a8c854e7e5a3ca71
dennyabrain <denny.george90@gmail.com>
2026-05-27 17:09:54 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import DecisionTree from './pages/decision-tree'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/unstructured-data-collection" element={<UnstructuredDataCollection />} />
